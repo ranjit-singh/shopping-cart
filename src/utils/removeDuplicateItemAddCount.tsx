7a136b0fc87d67160b6b8b4de6ce98e5099ceb2c
Ranjit Kumar Singh <ranjit.singh1@metlife.com>
2020-05-20 23:53:29 +0530

    eslint

--- src/utils/removeDuplicateItemAddCount.tsx
+++ src/utils/removeDuplicateItemAddCount.tsx
@@ -4,7 +4,7 @@ export default(cart: any[]) => {
     if (isEmpty(cart)) {
       return [];
     }
-    return (cart.filter((v: { count: number; id: any; }, i: number, a: any[]) => {
+    return (cart.filter((v: { count: number; id: any; }, _i: number, a: any[]) => {
       let count = 0;
       a.findIndex((t: { id: any; }) => { if (t.id === v.id) { count += 1; v.count = count; } }); return v;
     })).filter((v: { id: any; }, i: any, a: any[]) => a.findIndex((t: { id: any; }) => (t.id === v.id)) === i);
